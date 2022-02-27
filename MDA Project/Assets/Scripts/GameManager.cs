28750988c9017e311a11aabc634195df27f96008
George Rashed <goerge1110@gmail.com>
2022-02-27 13:40:17 +0200

    Added GameMenu with Live Map Feed.

--- MDA Project/Assets/Scripts/GameManager.cs
+++ MDA Project/Assets/Scripts/GameManager.cs
@@ -4,16 +4,59 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject GameMenuCanvas;
+    public bool GameMenuOpen = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        OnEscape(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape) && !GameMenuOpen)
+        {
+            OnEscape(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && GameMenuOpen)
+        {
+            OnEscape(false);
+        }
+    }
+
+    public void OnEscape(bool paused)
+    {
+        ChangeCursorMode(paused);
+        GameMenuMode(paused);
+    }
+
+    private void GameMenuMode(bool mode)
+    {
+        if (mode)
+        {
+            GameMenuCanvas.gameObject.SetActive(true);
+            GameMenuOpen = true;
+        }
+        else
+        {
+            GameMenuCanvas.gameObject.SetActive(false);
+            GameMenuOpen = false;
+        }
+    }
+
+    private void ChangeCursorMode(bool unlocked)
+    {
+        if (unlocked)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 }
