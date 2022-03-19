6cad8b0260c7352413f54bcb90d6c1bf69d85287
George Rashed <goerge1110@gmail.com>
2022-03-19 16:22:10 +0200

    Car Door Opening Mechanism Implemented with Animation.

--- MDA URP/Assets/Scripts/CarDoorCollision.cs
+++ MDA URP/Assets/Scripts/CarDoorCollision.cs
@@ -1,17 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarDoorCollision : MonoBehaviour
 {
+    public bool DoorOnLeft;
+    public bool DoorOpen = false;
     public bool SeatOccupied = false;
     public int SeatNum;
     public GameObject CollidedPlayer;
     public Transform SeatPos;
+
+    private Animator _doorAnimator;
     // Start is called before the first frame update
     void Start()
     {
-
+        _doorAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,22 +25,40 @@ public class CarDoorCollision : MonoBehaviour
         EnterExitVehicle();
     }
 
+    public void OpenCloseDoorToggle()
+    {
+        if (DoorOpen)
+        {
+            DoorOpen = false;
+            _doorAnimator.SetBool("IsDoorOpen", false);
+        }
+        else if (!DoorOpen)
+        {
+            DoorOpen = true;
+            _doorAnimator.SetBool("IsDoorOpen", true);
+        }
+    }
+
     private void EnterExitVehicle()
     {
         if (CollidedPlayer != null)
         {
-            if (Input.GetKeyDown(KeyCode.E) && !SeatOccupied)
+            if (DoorOpen)
             {
-                SeatOccupied = true;
-                CollidedPlayer.GetComponent<ThirdPersonMovement>().InControl = false;
-                CollidedPlayer.GetComponent<ThirdPersonMovement>().TogglePOV();
-            }
-            else if (Input.GetKeyDown(KeyCode.E) && SeatOccupied)
-            {
-                SeatOccupied = false;
-                CollidedPlayer.transform.position = gameObject.transform.position;
-                CollidedPlayer.GetComponent<ThirdPersonMovement>().InControl = true;
-                CollidedPlayer.GetComponent<ThirdPersonMovement>().TogglePOV();
+                if (Input.GetKeyDown(KeyCode.E) && !SeatOccupied)
+                {
+                    OpenCloseDoorToggle();
+                    SeatOccupied = true;
+                    CollidedPlayer.GetComponent<ThirdPersonMovement>().InControl = false;
+                    CollidedPlayer.GetComponent<ThirdPersonMovement>().TogglePOV();
+                }
+                else if (Input.GetKeyDown(KeyCode.E) && SeatOccupied)
+                {
+                    SeatOccupied = false;
+                    CollidedPlayer.transform.position = gameObject.transform.position;
+                    CollidedPlayer.GetComponent<ThirdPersonMovement>().InControl = true;
+                    CollidedPlayer.GetComponent<ThirdPersonMovement>().TogglePOV();
+                }
             }
 
             if (SeatOccupied)
