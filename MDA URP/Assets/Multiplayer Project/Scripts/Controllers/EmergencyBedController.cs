4acbd92e5516b4534f9a93522b0143edd54f7978
Daimnon <galdrorerez@gmail.com>
2022-06-11 21:10:37 +0300

    ScriptNameChange

--- MDA URP/Assets/Multiplayer Project/Scripts/Controllers/EmergencyBedController.cs
+++ MDA URP/Assets/Multiplayer Project/Scripts/Controllers/EmergencyBedController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class GurneyController : MonoBehaviour
+public class EmergencyBedController : MonoBehaviour
 {
     //public bool TakeOutBed;
     //public bool Folded;
